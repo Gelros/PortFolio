11033cbea7dcde5d696ced7c30bb41e746020724
ConstruisezUneAPI <guillaume.lacolley@gmail.com>
2023-12-06 16:49:45 +0100

    modif

--- src/components/Competence.tsx
+++ src/components/Competence.tsx
@@ -1,54 +1,46 @@
 import React from 'react';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faNodeJs } from '@fortawesome/free-brands-svg-icons';
+import {faGauge} from "@fortawesome/free-solid-svg-icons"
+import {faReact} from "@fortawesome/free-brands-svg-icons"
+import {faT} from "@fortawesome/free-solid-svg-icons"
+import {faGithub} from "@fortawesome/free-brands-svg-icons"
 const Competence = () => {
     return (
         <div className='comp-section' >
             <h1 data-aos="fade-down">Mes compétences</h1>
             <div className="comp-grille">
                 <div className="grille" data-aos="fade-up-right">
-                    <div className="grille-container">
+                    <div className="grille-container js">
                         <h3>Javascript</h3>
-                        <p className="percent">75%</p>
-                        <div className="note">
-                            <div className="note-2 a"></div></div>
+                        <FontAwesomeIcon icon={faNodeJs} />
                     </div>
 
-                    <div className="grille-container">
+                    <div className="grille-container node">
                         <h3>Node Js</h3>
-                        <p className="percent">65%</p>
-                        <div className="note">
-                            <div className="note-2 b"></div></div>
+                        <FontAwesomeIcon icon={faNodeJs} />
                     </div>
 
                     <div className="grille-container">
                         <h3>SEO</h3>
-                        <p className="percent">85%</p>
-                        <div className="note">
-                            <div className="note-2 c"></div></div>
+                        <FontAwesomeIcon icon={faGauge} />
                     </div>
 
                 </div>
                 <div className="grille" data-aos="fade-up-left">
                     <div className="grille-container">
                         <h3>React</h3>
-                        <p className="percent">80%</p>
-                        <div className="note">
-                            <div className="note-2 d"></div></div>
+                        <FontAwesomeIcon icon={faReact} />
                     </div>
 
                     <div className="grille-container">
                         <h3>TypeScript</h3>
-                        <p className="percent">50%</p>
-                        <div className="note">
-                            <div className="note-2 e"></div></div>
+                        <FontAwesomeIcon icon={faT} />
                     </div>
 
                     <div className="grille-container">
                         <h3>GitHub</h3>
-                        <p className="percent">80%</p>
-                        <div className="note">
-                            <div className="note-2 f"></div></div>
-
+                        <FontAwesomeIcon icon={faGithub} />
                     </div>
                 </div>
             </div>
