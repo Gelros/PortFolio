dc054b5befd1c2ace04845a96a7b7d8d6b769c2e
ConstruisezUneAPI <guillaume.lacolley@gmail.com>
2024-03-08 23:06:10 +0100

    ajust

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,33 +1,94 @@
-import React, { useState } from "react";
-import Pp from "./Pp"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faAddressBook } from '@fortawesome/free-solid-svg-icons';
-
-
+import { useState } from "react";
+import Pp from "./Pp";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAddressBook } from "@fortawesome/free-solid-svg-icons";
+import { Link, Element } from "react-scroll"
 
 const Nav = () => {
-    const [visible, setVisible] = useState(false)
-   
-    const mouseEnter = () => {
-        setVisible(true)
-    }
+  const [visible, setVisible] = useState(false);
 
-    const mouseLeave = () => {
-        setVisible(false)
-    }
+  const mouseEnter = () => {
+    setVisible(true);
+  };
 
-    return (
-        <div className={`navigation ` } onMouseEnter={mouseEnter} onMouseLeave={mouseLeave}>
-            
-            <Pp visible={visible}/> 
-                    <a className={`bloc-menu ${visible?"bloc-menu-visible" : "" }`}  href='#accueil'> <li>Accueil</li> </a>
-                    <a className={`bloc-menu ${visible?"bloc-menu-visible" : "" }`} href='#presentation'> <li>Présentation</li> </a>
-                    <a className={`bloc-menu ${visible?"bloc-menu-visible" : "" }`} href='#portfolio'> <li>Projets</li> </a>
-                    <a className={`bloc-menu ${visible?"bloc-menu-visible" : "" }`}  href='#competence'> <li>Compétences</li> </a>
-                    <a className={`bloc-menu ${visible?"bloc-menu-visible" : "" }`} href='#contact'> <li>Contact</li> </a>
-                    <a href="#contact" className={`bloc-menu ${visible?"bloc-menu-visible" : "" }`}><FontAwesomeIcon icon={faAddressBook} /></a>
-        </div>
-    );
+  const mouseLeave = () => {
+    setVisible(false);
+  };
+
+  return (
+    <div
+      className={`navigation `}
+      onMouseEnter={mouseEnter}
+      onMouseLeave={mouseLeave}
+    >
+      <Pp visible={visible} />
+      <Link
+        className={`bloc-menu ${visible ? "bloc-menu-visible" : ""}`}
+        to="accueil"
+        spy={true}
+        smooth={true}
+        offset={50}
+        duration={500}
+      >
+        {" "}
+        <li>Accueil</li>{" "}
+      </Link>
+      <Link
+        className={`bloc-menu ${visible ? "bloc-menu-visible" : ""}`}
+        to="presentation"
+        spy={true}
+        smooth={true}
+        offset={50}
+        duration={500}
+      >
+        {" "}
+        <li>Présentation</li>{" "}
+      </Link>
+      <Link
+        className={`bloc-menu ${visible ? "bloc-menu-visible" : ""}`}
+        to="portfolio"
+        spy={true}
+        smooth={true}
+        offset={50}
+        duration={500}
+      >
+        {" "}
+        <li>Projets</li>{" "}
+      </Link>
+      <Link
+        className={`bloc-menu ${visible ? "bloc-menu-visible" : ""}`}
+        to="competence"
+        spy={true}
+        smooth={true}
+        offset={50}
+        duration={500}
+      >
+        {" "}
+        <li>Compétences</li>{" "}
+      </Link>
+      <Link
+        className={`bloc-menu ${visible ? "bloc-menu-visible" : ""}`}
+        to="contact"
+        spy={true}
+        smooth={true}
+        offset={50}
+        duration={500}
+      >
+        {" "}
+        <li>Contact</li>{" "}
+      </Link>
+      <Link
+        to="contact"
+        spy={true}
+        smooth={true}
+        offset={50}
+        duration={500}
+        className={`bloc-menu ${visible ? "bloc-menu-visible" : ""}`}
+      >
+        <FontAwesomeIcon icon={faAddressBook} />
+      </Link>
+    </div>
+  );
 };
 
 export default Nav;
